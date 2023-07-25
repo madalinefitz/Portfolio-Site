ab44db591d56a466abc45f3e7b0f60ed91ce75b6
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-07-24 22:11:04 -0400

    edited about page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,27 +3,27 @@ import React from 'react'
 function AboutMe(){
     return(
         <div class="bg-[#FFC39E] h-screen px-52 py-20">
-            <div class="bg-[#FFECE1] bg-opacity-50 text-center p-5">
-                <h1 class="text-xl text-medium text-white">Experience</h1>
+            <div class="bg-[#FFECE1] bg-opacity-50 text-center p-5 m-10 text-[#FFECE1] shadow-md rounded-md">
+                <h1 class="text-3xl font-medium">Experience</h1>
                 <p>This is space for my background and experiences</p>
             </div>
-            <div class="flex flex-rows">
-                <div class="basis-1/2 bg-[#ED8A65] text-center p-5 m-10">
-                    <h1>Tech Stack</h1>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Python</li>
-                    <li>Flask</li>
+            <div class="flex flex-rows text-[#FFECE1]">
+                <div class="basis-1/2 bg-[#ED8A65] text-center p-5 m-10 shadow-md rounded-md">
+                    <h1 class="text-3xl font-medium">Tech Stack</h1>
+                    <p>JavaScript</p>
+                    <p>React</p>
+                    <p>Python</p>
+                    <p>Flask</p>
                 </div>
-                <div class="basis-1/2 bg-[#ED6565] text-center p-5 m-10">
-                    <h1>Skills</h1>
-                    <li>one</li>
-                    <li>two</li>
-                    <li>three</li>
+                <div class="basis-1/2 bg-[#ED6565] text-center p-5 m-10 shadow-md rounded-md">
+                    <h1 class="text-3xl font-medium">Skills</h1>
+                    <p>one</p>
+                    <p>two</p>
+                    <p>three</p>
                 </div>
             </div>
-            <div class="bg-[#FFEE98] text-center p-5 m-10">
-                <h1>For Fun</h1>
+            <div class="bg-[#FFEE98] text-center p-5 m-10 text-[#FFECE1] shadow-md rounded-md">
+                <h1 class="text-3xl font-medium">For Fun</h1>
                 <p>this is fun</p>
             </div>
 
