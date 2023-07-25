37bc1d10bd4bec1b3d5514c8af8f18a04a372c0a
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-07-24 22:19:24 -0400

    tech stack/skills edits

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,18 +8,24 @@ function AboutMe(){
                 <p>This is space for my background and experiences</p>
             </div>
             <div class="flex flex-rows text-[#FFECE1]">
-                <div class="basis-1/2 bg-[#ED8A65] text-center p-5 m-10 shadow-md rounded-md">
-                    <h1 class="text-3xl font-medium">Tech Stack</h1>
-                    <p>JavaScript</p>
-                    <p>React</p>
-                    <p>Python</p>
-                    <p>Flask</p>
+                <div class="basis-1/2 bg-[#ED8A65] m-10 shadow-md rounded-md">
+                    <h1 class="text-3xl text-center font-medium p-5">Tech Stack</h1>
+                    <div class="pl-10 pb-10">
+                        <p>JavaScript</p>
+                        <p>React</p>
+                        <p>Python</p>
+                        <p>Flask</p>
+                        <p>CSS</p>
+                        <p>TailwindCSS</p>
+                    </div>
                 </div>
-                <div class="basis-1/2 bg-[#ED6565] text-center p-5 m-10 shadow-md rounded-md">
-                    <h1 class="text-3xl font-medium">Skills</h1>
-                    <p>one</p>
-                    <p>two</p>
-                    <p>three</p>
+                <div class="basis-1/2 bg-[#ED6565] m-10 shadow-md rounded-md">
+                    <h1 class="text-3xl font-medium p-5 text-center">Skills</h1>
+                    <div class="pl-10 pb-10">
+                    <p>Time Management</p>
+                    <p>Communication</p>
+                    <p>Collaboration</p>
+                    </div>
                 </div>
             </div>
             <div class="bg-[#FFEE98] text-center p-5 m-10 text-[#FFECE1] shadow-md rounded-md">
