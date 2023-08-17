66f7961a912ea1bd81b4e4821d4b08438c3d662f
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-08-16 21:31:10 -0400

    added hustle card and modal state

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import hustle from './hustle.png'
 
 function Examples(){
+    const [showHustle, setShowHustle] = useState(false)
+
+    const handleShowHustle = () => {setShowHustle(!showHustle)}
+
     return(
         <div class="bg-[#FFC39E] h-screen">
-            <div class="text-4xl font-bold">Technical Work</div>
-            <div class="flex mb-4">
-                <div class="w-full max-w-sm w-full lg:max-w-full lg:flex mx-48">
-                    <div class="bg-[url('https://plus.unsplash.com/premium_photo-1673283381255-3b75088e3963?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')] h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
-                    </div>
-                    <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                        <div class="mb-8">
-                        <div class="text-gray-900 font-bold text-xl mb-2">Hustle.</div>
-                        <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+            <div class="text-6xl font-bold text-center text-[#FFECE1] p-10">Technical Work</div>
+                <div class="flex mb-4">
+                    <div class="w-full max-w-sm w-full lg:max-w-full lg:flex mx-48">
+                        <div >
+                            <img src={hustle} class="h-auto cursor-pointer" onClick={handleShowHustle}/>
+                        </div>
+                        <div class="bg-[#ED8A65] rounded-b shadow-lg lg:rounded-b-none lg:rounded-r p-10 flex flex-col justify-between leading-normal">
+                            <div class="mb-8">
+                            <div class="text-[#FFECE1] font-bold text-2xl mb-2">Hustle.</div>
+                            <p class="text-[#FFECE1] text-lg">User can organize and view schedule and estimated pay for multiple jobs using iOS app</p>
+                            <div class="text-[#FFECE1] p-5">
+                                <li>Built three models and associated database relationships using Python with Flask, serialize rules, and backpopulates</li>
+                                <li>Created effective RestAPI methods to perform full CRUD for information in databases </li>
+                                <li>Utilizing React Native CLI for iOS, built a user-friendly and efficient client side interface with multiple components, authorization using JWT, and useContext and useState hooks</li>
+                            </div>
                         </div>
                         <div class="flex items-center">
-                        <div class="text-sm">
-                            <p class="text-gray-900 leading-none">JavaScript, React Native, Python, Flask</p>
-                        </div>
+                            <div class="text-sm">
+                                <p class="text-[#FFECE1] leading-none">JavaScript, React Native, Python, Flask</p>
+                            </div>
                         </div>
                     </div>
                 </div>
